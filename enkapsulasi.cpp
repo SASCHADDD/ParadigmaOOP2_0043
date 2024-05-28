91c536ead7932973c495bdf0fe159498cbb10564
SASCHADDD <saschadanu23@gmail.com>
2024-05-28 18:39:22 +0700

    end

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -8,8 +8,23 @@ class remoteLampu{
         void setSaklarNo(int i, string value) {
             saklarNo[i] = value;
         }
-        string setSaklarNo(int i){
+        string getSaklarNo(int i){
             return saklarNo[i];
         }   
 };
 
+int main(){
+    remoteLampu LampuRumah;
+
+    LampuRumah.setSaklarNo(0, "Lampu teras Rumah");
+    LampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    LampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    LampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << LampuRumah.getSaklarNo(0) << endl;
+    cout << LampuRumah.getSaklarNo(1) << endl;
+    cout << LampuRumah.getSaklarNo(2) << endl;
+    cout << LampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
+}
